6f146a92ae7088801556e1ff4570a84870a0ae9d
mayavigg <112183799+mayavigg@users.noreply.github.com>
2022-08-26 19:54:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,15 @@
 from pyrogram import Client, filter
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-from pyrogram.types import CallbackQuery
-import randam
-import os
 
-PHOTO_LINK = [
- "Photo Link",
- "photo Link"
- ]
 
-Muhammed = Client(
-    "Pyrogram Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
+api_id = 13503794
+api_hash = "7c5edb66000bcb12d914019f87bffed4"
+bot_token = "5500972328:AAF8KG3bTj3ycmjEjpOg5Yo8oKivcrxnltI"
+
+bot = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
 )
 
-
-@Muhammed.on_message(filters.command("start")) 
-async def start_message(bot, message)
-    button = [[
-      InlineKeyboardButton("Mo Tech YT", callback_data="start")
-      ]]
-    await messages.reply_photo(
-        photo=random.choice(PHOTO_LINK),
-        text="Hello {message.from_user.mention}   Bro Sugamano",
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
-
-
-
-@Muhammad.on_callback_query()
-async def callback(bot, msg: CallbackQuery)
-    if msg.data == "start":
-        await message.message.edit(
-            text=" hello {msg.from_user.mention}  Start Text"
-        )
-
-
-
- Muhammed.run()
-
+print("bot started")
+app.run()
