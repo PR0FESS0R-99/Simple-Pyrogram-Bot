533c24eac69388499d0c3db29e1cf8e0a4fffe75
mayavigg <112183799+mayavigg@users.noreply.github.com>
2022-08-27 12:35:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def start(bot,msg):
      await asyncio.sleep(2)
      await bot.copy_message(-1001733967564,msg.chat.id,msg.id)
      await bot.copy_message(-1001755298903,msg.chat.id,msg.id)
-     await asyncio.sleep(3600)
+     await asyncio.sleep(1200)
      await bot.delete_messages(msg.chat.id,msg.id)
     
 @bot.on_message( filters.web_page)
