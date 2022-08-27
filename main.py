39e6fd0aa440e0ff269ba432e7dd07b97756144d
mayavigg <112183799+mayavigg@users.noreply.github.com>
2022-08-27 08:58:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def start(bot,msg):
 
 @bot.on_message(filters.video  | filters.document )
 async def start(bot,msg):
-     await bot.delete_messages(msg.chat.id,msg.id)
      await asyncio.sleep(1)
+     await bot.delete_messages(msg.chat.id,msg.id)
      await bot.copy_message(-1001733967564,msg.chat.id,msg.id)
      await bot.copy_message(-1001755298903,msg.chat.id,msg.id)
 
